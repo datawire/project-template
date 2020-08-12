5cfa0a1fd46453894df627fbb20a5743b82d98fd
matt-dw <54988209+matt-dw@users.noreply.github.com>
2020-08-12 12:24:24 -0400

    Matt Turner Testing

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var port = 8080
 var server = http.createServer(function (request, response) {
   console.log("ACCESS: " + request.url)
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('Hello World!\n')
+  response.end('Hello K8s World!\n')
 })
 
 server.listen(port)
